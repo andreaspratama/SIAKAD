56f5cb9103b8a4e1d0c0f91237cc0a8287481e82
Andreas Pratama <emailandreas19@gmail.com>
2020-07-08 20:48:43 +0700

    update tampilan

--- resources/views/pages/admin/auth/login.blade.php
+++ resources/views/pages/admin/auth/login.blade.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{url('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{url('backend/css/sb-admin-2.css')}}" rel="stylesheet">
 
 </head>
 
@@ -36,19 +36,22 @@
               {{-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> --}}
               <div class="col-lg-12">
                 <div class="p-5">
+                  <div class="bg-atas text-center mb-4">
+                    <img src="{{url('../../foto/bunayya.png')}}" alt="">
+                  </div>
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Sistem Informasi Akademik SD IT Bunayya</h1>
                   </div>
                   <form class="user" method="POST" action="/postlogin">
                     @csrf
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Username" name="username">
+                      <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukan Username" name="username">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Masukan Password" name="password">
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
-                      Login
+                      Masuk
                     </button>
                   </form>
                 </div>
